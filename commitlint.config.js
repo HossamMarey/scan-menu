205433cb15e125dd1002dfe99c4fe725d2533f06
Hossam <hmarey@ymail.com>
2025-08-11 18:38:10 +0300

    reset git

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+/**
+ * Commitlint configuration for conventional commits
+ * Enforces consistent commit message format
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation changes
+        'style', // Code style changes (formatting, etc.)
+        'refactor', // Code refactoring
+        'perf', // Performance improvements
+        'test', // Adding or updating tests
+        'chore', // Maintenance tasks
+        'ci', // CI/CD changes
+        'build', // Build system changes
+        'revert', // Reverting changes
+      ],
+    ],
+
+    'body-max-line-length': [2, 'always', 100],
+  },
+};
